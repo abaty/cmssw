edef16b6b542a5cec121e2d89adb945a509318b5
abaty <abaty@mit.edu>
2018-07-06 18:54:30 +0200

    pixel tracking integrated into pp reco

--- Configuration/StandardSequences/python/Reconstruction_cff.py
+++ Configuration/StandardSequences/python/Reconstruction_cff.py
@@ -151,12 +151,12 @@ highlevelreco = cms.Sequence(egammaHighLevelRecoPrePF*
 
 # XeXe data with pp reco
 from Configuration.Eras.Modifier_pp_on_XeXe_2017_cff import pp_on_XeXe_2017
+from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
 from RecoHI.HiCentralityAlgos.HiCentrality_cfi import hiCentrality
 from RecoHI.HiCentralityAlgos.HiClusterCompatibility_cfi import hiClusterCompatibility
+from RecoHI.HiTracking.HILowPtConformalPixelTracks_cfi import *
 _highlevelreco_HI = highlevelreco.copy()
-_highlevelreco_HI += hiCentrality
-_highlevelreco_HI += hiClusterCompatibility
-pp_on_XeXe_2017.toReplaceWith(highlevelreco, _highlevelreco_HI)
+(pp_on_XeXe_2017 | pp_on_AA_2018).toReplaceWith(highlevelreco, cms.Sequence(_highlevelreco_HI*hiCentrality*hiClusterCompatibility*hiConformalPixelTracksSequence_Phase1))
 
 # not commisoned and not relevant in FastSim (?):
 _fastSim_highlevelreco = highlevelreco.copyAndExclude([cosmicDCTracksSeq,muoncosmichighlevelreco])
